c67f7376f7856d132068b3f0f2b723ddcd990a51
Msasistro <90277664+Msasistro@users.noreply.github.com>
2022-02-21 02:16:00 +0200

    Update helloworld.cpp

--- helloworld.cpp
+++ helloworld.cpp
@@ -3,7 +3,7 @@
 
 int main(){
 
-cout<<"my name is Clement KS Mokoena \n username/nickname Msasistro /n "
+cout<<"my name is Clement KS Mokoena /n username/nickname Msasistro /n "
 <<"I am a beginner in the world of github and coding as a whole /n c++ is the language I use and its ayt /n"
 <<"I hope you'll land a hanf if you come across my profile on my journey to be a software developer";
 
